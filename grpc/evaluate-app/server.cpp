7b310a9146f7ace9c900efd11675fa131b933bf8
SreekarMouli <sreekar@everstage.com>
2023-10-03 22:59:10 +0530

    fix grpc

--- grpc/evaluate-app/server.cpp
+++ grpc/evaluate-app/server.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include <grpc++/grpc++.h>
 #include "communication.grpc.pb.h"
+#include "communication.pb.h"
+
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -17,7 +19,7 @@ class FormulaEvaluatorImpl final : public FormulaEvaluator::Service {
     Status EvaluateFormula(ServerContext* context, const FormulaRequest* request, FormulaResponse* response) override {
         // Implement your formula evaluation logic here based on the request
         // Set the result in the response
-        response->set_result(42);  // Replace with your actual calculation
+        response->set_result("42");  // Replace with your actual calculation
         return Status::OK;
     }
 };
@@ -37,6 +39,3 @@ int main() {
     
     return 0;
 }
-
-// g++ -std=c++11 -o server server.cpp communication.grpc.pb.cc communication.pb.cc -I/opt/homebrew/Cellar/grpc/1.59.0/include -I/opt/homebrew/Cellar/protobuf/24.3/include -I/opt/homebrew/Cellar/abseil/20230802.1/include -lgrpc++ -lgrpc -lprotobuf -m64
-// ./server
--- grpc/web-app/client.py
+++ grpc/web-app/client.py
@@ -1,8 +1,9 @@
+import json
 import grpc
 import communication_pb2
 import communication_pb2_grpc
 
-channel = grpc.insecure_channel('localhost:50051')  # Replace with the server address
+channel = grpc.insecure_channel('evaluate-app:50051')  # Replace with the server address
 
 formula_evaluator_stub = communication_pb2_grpc.FormulaEvaluatorStub(channel)
 
@@ -11,4 +12,58 @@ def send_request(ast, data):
     response = formula_evaluator_stub.EvaluateFormula(request)
     print("Result:", response.result)
 
-print("Running...")
+print("Begin")
+
+ast = {
+    "+": [
+        {
+            "type": "FUNCTION",
+            "id": "everMax",
+            "args": [
+                {
+                    "type": "VARIABLE",
+                    "id": "amount_1"
+                },
+                {
+                    "type": "VARIABLE",
+                    "id": "amount_2"
+                }
+            ]
+        },
+        {
+            "-": [
+                {
+                    "type": "VARIABLE",
+                    "id": "amount_3"
+                },
+                {
+                    "*": [
+                        {
+                            "type": "VARIABLE",
+                            "id": "amount_4"
+                        },
+                        {
+                            "type": "VARIABLE",
+                            "id": "amount_5"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+row = {
+    "id": "1",
+    "email": "user1@mail.com",
+    "sale_date": "2023-01-01",
+    "amount_1": 100,
+    "amount_2": 80,
+    "amount_3": 5,
+    "amount_4": 6,
+    "amount_5": 1,
+}
+
+send_request(json.dumps(ast), json.dumps(row))
+
+print("End")
